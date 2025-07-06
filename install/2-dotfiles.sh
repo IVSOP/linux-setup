cb40245c3009785dcebd47d8bdbdb1dc2b76c560
Ivan Ribeiro <ivan.ribeiro09s@gmail.com>
2025-07-06 19:50:10 +0100

    bugfix

--- install/2-dotfiles.sh
+++ install/2-dotfiles.sh
@@ -8,15 +8,22 @@ else
     echo "dotfiles/ already exists. Skipping clone."
 fi
 
+
+# copy all into .config
 sudo -u $USER_NAME mkdir -p .config
 cp dotfiles/* -r .config
-mv dotfiles/home_dotfiles/* .
-rm -r home_dotfiles
+
+# home_dotfiles and nemo should not be in .config
+rm -rf .config/home_dotfiles
+rm -rf .config/nemo
+
+# instead, they are copied somewhere else
+cp dotfiles/home_dotfiles/* .
 sudo -u $USER_NAME mkdir -p .local/share/applications
 cp -r dotfiles/nemo/* .local/share/applications/
 
 sudo -u $USER_NAME update-desktop-database /home/$USER_NAME/.local/share/applications
 
-DOTFILES_LOCATION="/home/$USER_NAME/dotfiles"
+export DOTFILES_LOCATION="/home/$USER_NAME/dotfiles"
 
 cd $SETUP_LOCATION
