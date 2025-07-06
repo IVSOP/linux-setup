0dcaead730d33863a6fcfb295576512b1c42994d
Ivan Ribeiro <ivan.ribeiro09s@gmail.com>
2025-07-06 19:59:38 +0100

    bugfix

--- install/5-terminal-shell.sh
+++ install/5-terminal-shell.sh
@@ -10,7 +10,13 @@ gsettings set org.cinnamon.desktop.default-applications.terminal exec alacritty
 # I think oh my zsh install already does this
 # chsh -s /usr/bin/zsh $USER_NAME
 
+SETUP_LOCATION=$(pwd)
+
+cd /home/$USER_NAME
+
 sudo -u $USER_NAME sh -c "$(sudo -u $USER_NAME curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # cp dotfiles/home_dotfiles/.zsh* .
 
 sudo -u $USER_NAME git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+
+cd $SETUP_LOCATION
