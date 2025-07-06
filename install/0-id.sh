1e7891fc3a08221674a32816a576ec2611b13c75
Ivan Ribeiro <ivan.ribeiro09s@gmail.com>
2025-07-06 19:22:48 +0100

    id bugfix

--- install/0-id.sh
+++ install/0-id.sh
@@ -4,12 +4,19 @@ pacman -S --noconfirm --needed gum openssh
 
 echo -e "\nEnter your info"
 export REAL_NAME=$(gum input --placeholder "Enter real name" --prompt "Real name> ")
+echo "real name: $REAL_NAME"
 export USER_NAME=$(gum input --placeholder "Enter user name" --prompt "User name> ")
+echo "user name: $USER_NAME"
 export USER_EMAIL=$(gum input --placeholder "Enter email address" --prompt "Email> ")
+echo "email: $USER_EMAIL"
 
-useradd -m -s /bin/bash $USER_NAME
+if ! id -u $USER_NAME >/dev/null 2>&1; then
+    useradd -g mygroup $USER_NAME
+fi
 passwd $USER_NAME
-groupadd wheel
+if ! getent group wheel >/dev/null; then
+    groupadd wheel
+fi
 usermod -aG wheel $USER_NAME
 
 sudo -u $USER_NAME ssh-keygen -t ed25519 -C "$USER_EMAIL"; break;;
