0d7c9cf03aaab3d3ce520c6facaa09bfed53a3dd
Ivan Ribeiro <ivan.ribeiro09s@gmail.com>
2025-07-06 22:47:13 +0100

    bugfix

--- post_setup.sh
+++ post_setup.sh
@@ -14,8 +14,17 @@ SETUP_LOCATION=$(pwd)
 
 cd /home/$USER_NAME
 
-sudo -u $USER_NAME git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+sudo -u "$USER_NAME" bash -c '
+    FZF_TAB_LOCATION="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fzf-tab"
+    if [ ! -d "$FZF_TAB_LOCATION" ]; then
+        sudo -u $USER_NAME git clone https://github.com/Aloxaf/fzf-tab "$FZF_TAB_LOCATION"
+    else
+        echo "fzf-tab/ already exists. Skipping clone."
+    fi
 
-sudo -u $USER_NAMEcp $DOTFILES_LOCATION/home_dotfiles/.zsh* .
+
+'
+
+sudo -u $USER_NAME cp $DOTFILES_LOCATION/home_dotfiles/.zsh* .
 
 cd $SETUP_LOCATION
