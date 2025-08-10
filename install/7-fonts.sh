fce41a8c67c35d163c3ee415681d020f4e78fed4
Ivan Ribeiro <ivan.ribeiro09s@gmail.com>
2025-08-10 13:49:26 +0100

    font bugfix

--- install/7-fonts.sh
+++ install/7-fonts.sh
@@ -1,4 +1,8 @@
 sudo -u $USER_NAME yay -S --noconfirm --needed \
-    gnu-free-fonts noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-ibm-plex nerd-fonts-jetbrains-mono nerd-fonts-symbols ttf-droid
+    gnu-free-fonts noto-fonts noto-fonts-cjk \
+    noto-fonts-emoji ttf-ibm-plex ttf-droid \
+    ttf-nerd-fonts-symbols
+
+# ttf-jetbrains-mono-nerd
 
 sudo -u $USER_NAME fc-cache -fv
