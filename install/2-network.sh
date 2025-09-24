05547da86d7ddb0119011031bfa04cf3299a49f3
Ivan Ribeiro <ivan.ribeiro09s@gmail.com>
2025-09-24 13:33:30 +0100

    nftables bugfix

--- install/2-network.sh
+++ install/2-network.sh
@@ -7,4 +7,8 @@ systemctl enable  --now nftables.service
 ufw enable
 systemctl enable  --now ufw
 ufw-docker install
+
+# for some reason ufw does not remove the default config, which basically restricts everything
+sudo nft delete table inet filter
+
 systemctl restart ufw
